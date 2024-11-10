8d22fc1eb9551c24ce7771e83e5c62fdedee9f1d
pjkee <pjkee99@hotmail.com>
2024-11-10 21:49:34 +0800

    add sliding effect to show/hide favourite pokemons

--- lib/features/pokemon/presentation/favourite/favourite_pokemon_list.dart
+++ lib/features/pokemon/presentation/favourite/favourite_pokemon_list.dart
@@ -12,39 +12,48 @@ class FavouritePokemonList extends ConsumerWidget {
 
     return favouritePokemons.when(
       data: (data) {
-        return Visibility(
-          visible: favouritePokemons.value?.isNotEmpty ?? false,
-          child: Padding(
-            padding: const EdgeInsets.all(12),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Your Favourites <3',
-                  style: Theme.of(context).textTheme.titleMedium,
+        // AnimatedContainer provides a sliding effect when show/hide the favourite list
+        return AnimatedContainer(
+          duration: const Duration(milliseconds: 500),
+          height: data.isNotEmpty ? 180 : 0,
+          curve: Curves.easeInOut,
+          child: ClipRect(
+            child: Align(
+              alignment: Alignment.topCenter,
+              heightFactor: data.isNotEmpty ? 1 : 0, // ensure smoother animation
+              child: Padding(
+                padding: const EdgeInsets.all(12),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Your Favourites <3',
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    const SizedBox(height: 4),
+                    SizedBox(
+                      height: 120,
+                      child: ListView.separated(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: favouritePokemons.value!.length,
+                        itemBuilder: (ctx, index) {
+                          return FavouritePokemonListItem(
+                              name: favouritePokemons.value![index]);
+                        },
+                        separatorBuilder: (ctx, index) {
+                          return const SizedBox(width: 8);
+                        },
+                      ),
+                    ),
+                  ],
                 ),
-                const SizedBox(height: 4),
-                SizedBox(
-                  height: 120,
-                  child: ListView.separated(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: favouritePokemons.value!.length,
-                    itemBuilder: (ctx, index) {
-                      return FavouritePokemonListItem(
-                          name: favouritePokemons.value![index]);
-                    },
-                    separatorBuilder: (ctx, index) {
-                      return const SizedBox(width: 8);
-                    },
-                  ),
-                ),
-              ],
+              ),
             ),
           ),
         );
       },
       loading: () => const Text('loading'),
-      error: (err, stackTrace) =>  Text(err.toString()),
+      error: (err, stackTrace) => Text(err.toString()),
     );
   }
 }
